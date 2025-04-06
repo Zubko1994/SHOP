e9920d819edd195bb9c8880bd135a044d7384455
Zubko1994 <zubko721994@gmail.com>
2025-04-06 20:31:58 +0300

    component button

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,81 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import btn from "./components/btn.vue";
+
+export default {
+    name: "app",
+    components: {
+        btn,
+    },
+    data() {
+        return {};
+    },
+    methods: {
+        clickBtn() {
+            console.log("juhgfd");
+        },
+    },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div id="app">
+        <div class="line">
+            <btn @click="clickBtn" />
+            <btn title="Выбрать товар" kind="primary" />
+            <btn title="Купить в 1 клик" kind="buying" />
+            <btn title="Смотреть больше товаров" kind="primary" />
+            <btn title="Смотреть больше брендов" kind="primary" />
+        </div>
+    </div>
+    <div class="line">
+        <btn title="6 л." kind="quantity" />
+        <btn title="18 л." kind="quantity" />
+        <btn title="30 л." kind="quantity" />
+        <btn title="12 л." kind="quantity-active" />
+        <btn title="85 г." kind="quantity-active" />
+        <btn title="1 шт." kind="quantity-active" />
+        <btn title="0,2 кг." kind="quantity" />
+        <btn title="1,2 кг." kind="quantity" />
+        <btn title="2 кг." kind="quantity" />
+        <btn title="4 кг." kind="quantity" />
+        <btn title="10 кг." kind="quantity" />
+        <btn title="Задать вес" kind="quantity" />
+        <btn title="0,4 кг." kind="quantity-active"/>
+    </div>
+    <btn title="" @click="clickBtn" kind="basket"/>
+    <btn title="" @click="clickBtn" kind="basket-adding"/>
+    <btn title="" @click="clickBtn" kind="indicator-left"/>
+    <btn title="" @click="clickBtn" kind="indicator-right"/>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+:root {
+    --white: #ffffff;
+    --default-hover: #dad0d0a9;
+    --text-default: #202223;
+    --highlight: #00a0ac;
+    --highlight-hover: #087b83;
+    --highlight-active: #449da7;
+    --surface-neutral: #e4e5e7;
+    --border-default: 1px solid #8c9196;
+    --border-highlight: 1px solid #00a0ac;
+    --gradient-button: linear-gradient(
+            180deg,
+            #babfc3 86.46%,
+            #bbbfc3 100%);
+    --bg-button: #F6F6F7;
+
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+#app {
+    font-family: "SFProText";
+    font-weight: 400;
+    color: var(--color-text-default);
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.line {
+    display: flex;
+    flex-direction: row;
+    gap: 5px;
+    align-items: center;
 }
 </style>
---
+++ src/components/btn.vue
@@ -0,0 +1,131 @@
+<script>
+import { ref } from "vue";
+
+export default {
+    name: "btn",
+    props: {
+        title: {
+            type: String,
+            default: "Перейти к выбору товара",
+        },
+        kind: {
+            type: String,
+            default: "",
+        },
+    },
+    data() {
+        return {};
+    },
+    method: {
+        click() {
+            this.$emit("click");
+        },
+    },
+};
+</script>
+
+<template>
+    <div :class="['btn', `btn_${kind}`, `btn_${borderBtn}`]" @click="click">
+        <span>{{ title }}</span>
+    </div>
+</template>
+
+<style lang="scss">
+.btn {
+    color: var(--text-default);
+    font-family: "SFProText";
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    padding: 8px 16px;
+    border: var(--border-default);
+    cursor: pointer;
+    margin-bottom: 10px;
+    background: var(--white);
+    display: inline-block;
+    &:hover {
+        background-color: var(--default-hover);
+    }
+    &_primary {
+        background-color: var(--highlight);
+        box-shadow: 0px -1px 0px 0px #00000033 inset;
+        box-shadow: 0px 1px 0px 0px #00000014;
+        color: var(--white);
+        padding: 12px 24px;
+        border: none;
+        display: inline-block;
+        &:hover {
+            background-color: var(--highlight-hover);
+        }
+    }
+    &_buying {
+        border: var(--border-highlight);
+    }
+    &_quantity {
+        background: var(--surface-neutral);
+        color: var(--text-default);
+        padding: 2px 8px;
+        display: inline-block;
+        &:hover {
+            background: var(--default-hover);
+        }
+    }
+    &_quantity-active {
+        background: var(--highlight-active);
+        color: var(--white);
+        padding: 2px 8px;
+        display: inline-block;
+    }
+    &_basket {
+        border: 1px solid;
+        border-image-source: var(--gradient-button);
+        padding: 8px 14px;
+        background-image: url("/src/assets/image/basket.png");
+        width: 60px;
+        height: 36px;
+        background-repeat: no-repeat;
+        background-position: center;
+        border: 1px solid #babfc3;
+        border-radius: 4px;
+        margin-right: 10px;
+    }
+    &_basket-adding {
+        border-radius: 5px;
+        border: 1px solid;
+        border-image-source: var(--gradient-button);
+        padding: 5px 12px;
+        background-image: url("/src/assets/image/basketadding.png");
+        width: 54px;
+        height: 30px;
+        background-repeat: no-repeat;
+        background-position: center;
+        border: 1px solid #babfc3;
+        box-shadow: 0px 1px 0px 0px #0000000d;
+        margin-right: 10px;
+    }
+
+    &_indicator-left {
+        width: 40px;
+        height: 40px;
+        border-radius: 40px;
+        padding: 10px;
+        background-image: url("/src/assets/image/Primaryfillleft.svg");
+        background-repeat: no-repeat;
+        background-position: center;
+        margin-right: 10px;
+        background-color: var(--bg-button);
+    }
+    &_indicator-right {
+        width: 40px;
+        height: 40px;
+        border-radius: 40px;
+        padding: 10px;
+        background-image: url("/src/assets/image/Primaryfill.svg");
+        background-repeat: no-repeat;
+        background-position: center;
+        margin-right: 10px;
+        background-color: var(--bg-button);
+    }
+}
+</style>
